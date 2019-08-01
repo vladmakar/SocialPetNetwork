f06701019d05fdc61806f770631f851a525c466f
vladmakar <vladikmakar2745@gmail.com>
2019-08-01 16:08:18 -0400

    Initial Commit

---
+++ SocialPetNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialPetNetwork
+//
+//  Created by Влад Макар on 2019-08-01.
+//  Copyright © 2019 Влад Макар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
